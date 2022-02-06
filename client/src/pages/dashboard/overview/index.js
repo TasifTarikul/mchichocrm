ef1391c0bc84a38f25e5b9ce62449ada90a2feb0
raihan <tasiftarikul@gmail.com>
2022-02-06 19:22:40 +0600

    client index

---
+++ client/src/pages/dashboard/overview/index.js
@@ -0,0 +1,11 @@
+import{ Card, Button, Row} from 'antd';
+import { GridContent } from '@ant-design/pro-table';
+
+export default() => {
+  return(
+    <GridContent>
+      <Card title="My Projects">
+      </Card>
+    </GridContent>
+  );
+};
